1206afe00811d1b963582287873646b0965a5d2a
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-02-22 23:25:37 +0700

    visualizing points

---
+++ src/visual.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import main
+
+def visualize(listOfPoints) :
+    fig = plt.figure(figsize=(11,7))
+    ax = fig.add_subplot(111, projection='3d')
+
+    for i in range (len(listOfPoints)) :
+        x = listOfPoints[i][0]
+        y = listOfPoints[i][1]
+        z = listOfPoints[i][2]
+
+        ax.scatter(x,y,z)
+    
+    plt.show()
