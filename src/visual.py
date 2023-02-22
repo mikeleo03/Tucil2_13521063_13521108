13ddde243b645b37a4448a36cc6b08ebce6f8465
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-02-22 23:50:08 +0700

    visualize the shortest

--- src/visual.py
+++ src/visual.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import main
+
 
 def visualize(listOfPoints) :
     fig = plt.figure(figsize=(11,7))
@@ -9,6 +11,27 @@ def visualize(listOfPoints) :
         y = listOfPoints[i][1]
         z = listOfPoints[i][2]
 
-        ax.scatter(x,y,z)
+        ax.scatter(x,y,z, color='red')
     
+    plt.show()
+
+def visualizeMinimum(listOfPoints) :
+    fig = plt.figure(figsize=(11,7))
+    ax = fig.add_subplot(111, projection='3d')
+
+    for i in range (len(listOfPoints)) :
+        x = listOfPoints[i][0]
+        y = listOfPoints[i][1]
+        z = listOfPoints[i][2]
+
+        ax.scatter(x,y,z, color='black')
+    
+    point1, point2, dist = main.bruteForce(listOfPoints)
+
+    ax.scatter(point1[0],point1[1],point1[2], color='red')
+    ax.scatter(point2[0],point2[1],point2[2], color='red')
+
+    # How to plot the line
+    # ax.plot([point1[0],point2[0]],[point1[1],point2[1]],[point1[2],point2[2]],color='green')
+
     plt.show()
