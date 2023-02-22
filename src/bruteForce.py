bedae5d40c709e4517dd688e355d2979e869fb38
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-02-22 23:05:24 +0700

    brute force distance general

--- src/bruteForce.py
+++ src/bruteForce.py
@@ -11,3 +11,15 @@ def bruteForce(listOfPoints) :
                 b = listOfPoints[j]
                 
     return shortest
+
+def bruteForceDistGeneral(listOfPoints, dimention) :
+    shortest = main.EuclideanDistGeneral(listOfPoints[0], listOfPoints[1])
+
+    for i in range (len(listOfPoints)) :
+        for j in range(len(listOfPoints)) :
+            if (i != j) and (main.EuclideanDistGeneral(listOfPoints[i], listOfPoints[j]) <= shortest):
+                shortest = main.EuclideanDistGeneral(listOfPoints[i], listOfPoints[j])
+                a = listOfPoints[i]
+                b = listOfPoints[j]
+                
+    return a, b, shortest
